bb7b949212ae50b70af1f58e1ff060ecab4d799a
Saabane <43517321+hitSKYGAM3S@users.noreply.github.com>
2024-03-02 18:12:55 +0100

    Simulate a rps game a simulation of one round of rps and gets return for each round depending on what user predetermined choice

--- javascript.js
+++ javascript.js
@@ -20,4 +20,29 @@ function getComputerChoice()
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    
+    if ((playerSelection === rock && computerSelection === rock) ||
+        (playerSelection === paper && computerSelection === paper) ||
+        (playerSelection === scissors && computerSelection === scissors)) {
+        return "It's a tie";
+    } else if (playerSelection === paper && computerSelection === rock) 
+    {
+        return "You win, Paper beats rock";
+    }else if (playerSelection === scissors && computerSelection === paper) 
+    {
+        return "You win, Scissors beats paper";
+    }else if (playerSelection === rock && computerSelection === scissors) {
+        return "You win, Rock beats scissors";
+    }else if (playerSelection === paper && computerSelection === scissors) {
+        return "You lose, Scissors beats paper";
+    }else if (playerSelection === scissors && computerSelection === rock) {
+        return "You lose, Rock beats scissors";
+    }else {
+        return "You lose, Paper beats rock";
+    }
+  }
+
+const playerSelection = rock;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
